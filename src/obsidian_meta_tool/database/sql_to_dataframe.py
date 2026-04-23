98b8fead33c289191f6b2e731ba7f7aeac08c97b
cff100 <caiofelix190799@gmail.com>
2026-04-23 17:19:06 -0300

    Transforma caminhos do dataframe de str para objetos Path

--- src/obsidian_meta_tool/database/sql_to_dataframe.py
+++ src/obsidian_meta_tool/database/sql_to_dataframe.py
@@ -1,5 +1,6 @@
-import pandas as pd
 from sqlalchemy import create_engine
+from pathlib import Path
+import pandas as pd
 
 from obsidian_meta_tool.config.paths import SQL_DATABASE_PATH
 
@@ -7,6 +8,16 @@ from obsidian_meta_tool.config.paths import SQL_DATABASE_PATH
 engine = create_engine(f'sqlite:///{SQL_DATABASE_PATH}')
 
 # 2. Lê uma tabela inteira ou uma query específica
-df = pd.read_sql("SELECT * FROM files WHERE extension = '.md'", engine)
+df = pd.read_sql("SELECT * FROM files", engine)
 
-print(df.head()) # Exibe as primeiras 5 linhas
+
+def df_filepath_to_path_object(df: pd.DataFrame) -> pd.DataFrame:
+    df["filepath"] = df["filepath"].apply(Path) # type: ignore
+    return df
+    
+
+if __name__ == "__main__":
+    # Exemplo de uso
+    print(df.head())  # Exibe as primeiras 5 linhas do DataFrame
+    print(df_filepath_to_path_object(df).head())
+    print(df_filepath_to_path_object(df).info())
