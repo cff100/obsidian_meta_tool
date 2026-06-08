f45e75dc9bb738cafee4927379ad95870ed8545c
cff100 <caiofelix190799@gmail.com>
2026-06-08 19:54:59 -0300

    Revisa uso dos utils

--- src/obsidian_meta_tool/utils/extensions.py
+++ src/obsidian_meta_tool/utils/extensions.py
@@ -1,19 +1,19 @@
 
-from pathlib import Path
+# from pathlib import Path
 
 
-def is_md(path: Path) -> bool:
+# def is_md(path: Path) -> bool:
 
-    return path.suffix == ".md"
+#     return path.suffix == ".md"
 
-def is_text_file(path: Path) -> bool:
+# def is_text_file(path: Path) -> bool:
 
-    if path.is_dir:
-        print("A")
-        return False
-    elif path.suffix not in [".md", ".txt"]:
-        print("B")
-        return False
-    else: 
-        print("C")
-        return True
+#     if path.is_dir:
+#         print("A")
+#         return False
+#     elif path.suffix not in [".md", ".txt"]:
+#         print("B")
+#         return False
+#     else: 
+#         print("C")
+#         return True
