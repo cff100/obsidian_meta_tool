55640c354ab3f3342730efd96778c6918bf9ea45
cff100 <caiofelix190799@gmail.com>
2026-07-14 12:50:05 -0300

    Termina a validação de extensões e adiciona a funcionalidade de criar csv com as linhas invalidas do dataframe para cada tipo de validação

--- src/obsidian_meta_tool/database/validate_database.py
+++ src/obsidian_meta_tool/database/validate_database.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from typing import Optional
 
 from obsidian_meta_tool.database.notes_categories_creation import CategoriesNames
+from obsidian_meta_tool.io.save_data import save_dataframe_as_csv
+from obsidian_meta_tool.config.paths import DataPaths
 
 def validate_database(df: pd.DataFrame) -> bool:
     """
     Validates the database by checking if the note path and note filename are valid.
     """
-    valid = path_validation(df) and filename_validation(df)
+    valid = all([path_validation(df), filename_validation(df), extension_validation(df)])
     if valid:
         print("Database validation successful.")
     else:
@@ -18,7 +19,12 @@ def path_validation(df: pd.DataFrame) -> bool:
     """
     Validates if the note path is not None.
     """
-    return not df[CategoriesNames.NOTE_PATH.value].isnull().any()
+    valid = not df[CategoriesNames.NOTE_PATH.value].isnull().any()
+    if not valid:
+        print("Validation failed: Some note paths are None.")
+        invalid_paths_df = df[df[CategoriesNames.NOTE_PATH.value].isnull()]
+        save_dataframe_as_csv(invalid_paths_df, DataPaths.DATA_FOLDER / "invalid_paths.csv")
+    return valid
 
 def filename_validation(df: pd.DataFrame) -> bool:
     """
@@ -27,17 +33,25 @@ def filename_validation(df: pd.DataFrame) -> bool:
     df_md = df[df[CategoriesNames.NOTE_EXTENSION.value] == ".md"]
     df_non_md = df[df[CategoriesNames.NOTE_EXTENSION.value] != ".md"]
 
-    validated = True
+    valid = True
 
     if df_md[CategoriesNames.NOTE_FILENAME.value].isnull().any():
         print("Validation failed: Some .md files have a None filename.")
-        validated = False
+        valid = False
+
+        invalid_filenames_df = df_md[df_md[CategoriesNames.NOTE_FILENAME.value].isnull()]
+        save_dataframe_as_csv(invalid_filenames_df, DataPaths.DATA_FOLDER / "invalid_filenames.csv")
+        
     if df_non_md[CategoriesNames.NOTE_FILENAME.value].notnull().any():
         print("Validation failed: Some non-.md files have a non-None filename.")
-        validated = False
-    return validated
+        valid = False
 
-def extension_validation(df: pd.DataFrame) -> tuple[bool, Optional[pd.DataFrame]]:
+        invalid_non_md_filenames_df = df_non_md[df_non_md[CategoriesNames.NOTE_FILENAME.value].notnull()]
+        save_dataframe_as_csv(invalid_non_md_filenames_df, DataPaths.DATA_FOLDER / "invalid_non_md_filenames.csv")
+        
+    return valid
+
+def extension_validation(df: pd.DataFrame) -> bool:
     """
     Validates if the note extension is None for folders and not None for files.
     """
@@ -45,10 +59,20 @@ def extension_validation(df: pd.DataFrame) -> tuple[bool, Optional[pd.DataFrame]
     folders_df = df[df[CategoriesNames.NOTE_PATH.value].apply(lambda x: x.is_dir())]
     files_df = df[df[CategoriesNames.NOTE_PATH.value].apply(lambda x: x.is_file())]
 
+    valid = True
+
     if folders_df[CategoriesNames.NOTE_EXTENSION.value].notnull().any():
         print("Validation failed: Some folders have a non-None extension.")
-        return False, folders_df[folders_df[CategoriesNames.NOTE_EXTENSION.value].notnull()]
+        valid = False
+
+        invalid_folders_df = folders_df[folders_df[CategoriesNames.NOTE_EXTENSION.value].notnull()]
+        save_dataframe_as_csv(invalid_folders_df, DataPaths.DATA_FOLDER / "invalid_folders.csv")
+
     if files_df[CategoriesNames.NOTE_EXTENSION.value].isnull().any():
         print("Validation failed: Some files have a None extension.")
-        return False, files_df[files_df[CategoriesNames.NOTE_EXTENSION.value].isnull()]
-    return True, None
+        valid = False
+
+        invalid_files_df = files_df[files_df[CategoriesNames.NOTE_EXTENSION.value].isnull()]
+        save_dataframe_as_csv(invalid_files_df, DataPaths.DATA_FOLDER / "invalid_files.csv")
+        
+    return valid
